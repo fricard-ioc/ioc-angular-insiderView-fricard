facbe3573c40862ffd9f6aa2788c9109bd02c760
fricard <odracir92@hotmail.com>
2026-03-21 19:35:25 +0100

    EAC1: components modificats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
 export class AppComponent {
-  title = 'ioc-angular-insiderView-fricard';
+  constructor() {
+    console.log('Inicialització OK: Initialitzant tracking');
+  }
 }
