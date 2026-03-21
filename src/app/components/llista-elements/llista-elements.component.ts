23d9db5e2a1375e5d1925bd6cc78b2064f94ca6d
fricard <odracir92@hotmail.com>
2026-03-21 20:34:59 +0100

    EAC2: Components creats

---
+++ src/app/components/llista-elements/llista-elements.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-llista-elements',
+  standalone: true,
+  imports: [],
+  templateUrl: './llista-elements.component.html',
+  styleUrl: './llista-elements.component.scss'
+})
+export class LlistaElementsComponent {
+
+}
