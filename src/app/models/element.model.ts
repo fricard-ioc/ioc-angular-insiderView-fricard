da6e549ffc5bb71b17b1d61de4c9aa258eb33c93
fricard <odracir92@hotmail.com>
2026-04-20 19:11:09 +0200

    Ex2. Modificacions 'element.model' amb nous camps obligatoris

--- src/app/models/element.model.ts
+++ src/app/models/element.model.ts
@@ -1,10 +1,29 @@
-export interface Element {
+export interface ElementApiResponse {
   id: string;
   nom: string;
-  ticker: string;
+  descripcio: string;
+  categoria: string;
   preu: number;
+  imatge: string;
+  popular: boolean;
+  stock: number;
+
+  ticker: string;
   moneda: string;
+  activitat_recent: boolean;
+}
 
+export interface ElementCataleg {
+  id: string;
+  titol: string;
+  descripcio: string;
+  categoria: string;
+  preu: number;
+  imatgeUrl: string;
+  esPopular: boolean;
+  unitats: number; 
 
-  activitatRecent?: boolean;
+  ticker: string;
+  moneda: string;
+  activitatRecent: boolean;
 }
