ef629123193118a6da06663bed8682b1efb84c4f
fricard <odracir92@hotmail.com>
2026-04-21 23:28:22 +0200

    Ex3. Comptador de favorits mostrat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CatalegPageComponent } from './components/cataleg-page/cataleg-page.component';
 import { FormulariCercaComponent } from './components/formulari-cerca/formulari-cerca.component';
+import { inject } from '@angular/core';
+import { PreferitsService } from './serveis/preferits.service';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,6 @@ import { FormulariCercaComponent } from './components/formulari-cerca/formulari-
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public preferitsService = inject(PreferitsService)
   title = 'ioc-angular-insiderView-fricard';
 }
